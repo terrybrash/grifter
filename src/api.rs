76ca525d4384544648962af08d04901651a058a0
Terry Brashaw <terrybrashaw@gmail.com>
2020-05-09 16:46:02 -0400

    Consolidate games+genres+themes into a single "catalog" object

--- src/api.rs
+++ src/api.rs
@@ -12,6 +12,7 @@ use rocket::State;
 use rocket::{get, routes};
 use rocket_contrib::serve::StaticFiles;
 use rocket_cors::{AllowedOrigins, CorsOptions};
+use serde::Serialize;
 use std::io;
 use std::io::Write;
 
@@ -22,10 +23,15 @@ pub fn gzip(mut bytes: Vec<u8>) -> io::Result<Vec<u8>> {
 }
 
 struct Model {
-    games: Vec<u8>,
-    genres: Vec<u8>,
-    themes: Vec<u8>,
+    catalog: Vec<u8>,
     index: Vec<u8>,
+}
+
+#[derive(Clone, Serialize)]
+struct Catalog {
+    games: Vec<Game>,
+    genres: Vec<igdb::Genre>,
+    themes: Vec<igdb::Theme>,
 }
 
 pub fn start(config: &Config, games: Vec<Game>) {
@@ -37,20 +43,19 @@ pub fn start(config: &Config, games: Vec<Game>) {
     .to_cors()
     .unwrap();
 
-    let games = gzip(serde_json::to_vec(&games).unwrap()).unwrap();
-
     let mut genres = igdb::get_genres(&config.igdb_key).unwrap();
     genres.sort_by(|a, b| a.name.cmp(&b.name));
-    let genres = gzip(serde_json::to_vec(&genres).unwrap()).unwrap();
-
     let mut themes = igdb::get_themes(&config.igdb_key).unwrap();
     themes.sort_by(|a, b| a.name.cmp(&b.name));
-    let themes = gzip(serde_json::to_vec(&themes).unwrap()).unwrap();
 
-    let model = Model {
+    let catalog = Catalog {
         games,
         genres,
         themes,
+    };
+
+    let model = Model {
+        catalog: gzip(serde_json::to_vec(&catalog).unwrap()).unwrap(),
         index: gzip(include_bytes!(concat!(env!("OUT_DIR"), "/index.html"))[..].into()).unwrap(),
     };
 
@@ -58,7 +63,7 @@ pub fn start(config: &Config, games: Vec<Game>) {
         .attach(cors)
         .manage(model)
         .mount("/games", StaticFiles::from(&config.root))
-        .mount("/", routes![get_index, get_games, get_genres, get_themes])
+        .mount("/", routes![get_index, get_catalog])
         .launch();
 }
 
@@ -75,19 +80,9 @@ impl<'r, R: Responder<'r>> Responder<'r> for EncodedContent<R> {
     }
 }
 
-#[get("/themes")]
-fn get_themes(model: State<Model>) -> EncodedContent<Vec<u8>> {
-    EncodedContent(ContentType::JSON, Encoding::Gzip, model.themes.clone())
-}
-
-#[get("/genres")]
-fn get_genres(model: State<Model>) -> EncodedContent<Vec<u8>> {
-    EncodedContent(ContentType::JSON, Encoding::Gzip, model.genres.clone())
-}
-
-#[get("/games")]
-fn get_games(model: State<Model>) -> EncodedContent<Vec<u8>> {
-    EncodedContent(ContentType::JSON, Encoding::Gzip, model.games.clone())
+#[get("/catalog")]
+fn get_catalog(model: State<Model>) -> EncodedContent<Vec<u8>> {
+    EncodedContent(ContentType::JSON, Encoding::Gzip, model.catalog.clone())
 }
 
 #[get("/")]
