dcd521db689b5c5c4e0175f21d354f5aacd3eabe
Terry Brashaw <terrybrashaw@gmail.com>
2021-10-03 00:51:53 -0400

    Add client release bat script for performance testing

---
+++ client-web/release.bat
@@ -0,0 +1,9 @@
+elm-live^
+    src/Main.elm^
+    --start-page index.html^
+    --proxy-host http://127.0.0.1:39090/api/^
+    --proxy-prefix /api/^
+    --pushstate^
+    --^
+    --output=assets/elm.js^
+    --optimize^
